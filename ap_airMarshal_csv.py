2384426fa38a3103f008a80c55a7169cd3fe1349
ulisesGutierrez <techwithulises@gmail.com>
2023-07-10 12:26:00 -0600

    new

---
+++ ap_airMarshal_csv.py
@@ -0,0 +1,57 @@
+import os
+import json
+import csv
+
+input_directory = 'ext'
+output_directory = 'csvs'
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+columns = ['id', 'name', 'ssid', 'bssids', 'device', 'channels', 'firstSeen', 'lastSeen']
+csv_data = []
+
+total_files = len([filename for filename in os.listdir(input_directory) if filename.endswith('.json')])
+
+json_count = 0
+csv_count = 0
+
+for filename in os.listdir(input_directory):
+    if filename.endswith('.json'):
+        json_count += 1
+
+        with open(os.path.join(input_directory, filename), 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        if isinstance(data, list):
+            for item in data:
+                if 'channels' in item:
+                    channels = item['channels']
+                    if isinstance(channels, str):
+                        channels = channels.replace(',', '-')
+                        item['channels'] = channels
+
+                bssids = ', '.join([bssid['bssid'] for bssid in item.get('bssids', [])])
+                devices = ', '.join([device['device'] for bssid in item.get('bssids', []) for device in bssid.get('detectedBy', [])])
+
+                row_data = {column: item.get(column, '') for column in columns}
+                row_data['bssids'] = bssids
+                row_data['device'] = devices
+
+                csv_data.append(row_data)
+
+        if json_count % 15 == 0 or json_count == total_files:
+            csv_count += 1
+            csv_filename = f'csv{csv_count}.csv'
+            csv_path = os.path.join(output_directory, csv_filename)
+            with open(csv_path, 'w', newline='', encoding='utf-8') as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=columns)
+                writer.writeheader()
+                writer.writerows(csv_data)
+                csv_data = []
+
+        progress = int((json_count / total_files) * 100)
+        progress_str = f'[{progress}%]'
+        print(f'\rProgress: {progress_str}', end='')
+
+print("\nSuccess")
---
+++ ba_add_id_name_json.py
@@ -0,0 +1,38 @@
+import os
+import json
+from ai_network_ids_names import idname
+
+# Ruta de la carpeta que contiene los archivos JSON
+carpeta = 'ext'
+
+# Iterar sobre cada archivo en la carpeta
+for archivo in os.listdir(carpeta):
+    if archivo.endswith('.json'):
+        # Obtener el ID del archivo
+        id_archivo = os.path.splitext(archivo)[0]
+        
+        # Buscar el nombre correspondiente en el array idname
+        nombre_archivo = None
+        for item in idname:
+            if item['id'] == id_archivo:
+                nombre_archivo = item['name']
+                break
+        
+        if nombre_archivo:
+            # Leer el archivo JSON
+            ruta_archivo = os.path.join(carpeta, archivo)
+            with open(ruta_archivo, 'r') as f:
+                contenido = json.load(f)
+            
+            # Añadir los campos id y name a cada diccionario
+            for diccionario in contenido:
+                diccionario['id'] = id_archivo
+                diccionario['name'] = nombre_archivo
+            
+            # Guardar el archivo actualizado
+            with open(ruta_archivo, 'w') as f:
+                json.dump(contenido, f, indent=4)
+            
+            print(f"Se añadieron los campos id y name al archivo: {archivo}")
+        else:
+            print(f"No se encontró el nombre correspondiente para el ID {id_archivo}")
